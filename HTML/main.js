b049aa6e84525bd5145d6622730b6d3cbf79982e
Santhosh Kumar Gnanamani <gsaku0091@gmail.com>
2018-05-14 19:30:52 +0530

    Added Embedding html and local storage

--- HTML/main.js
+++ HTML/main.js
@@ -1,26 +1,41 @@
-var contactsObj = [];
+var contactsObj = {};
 var contacts = null;
-//window.addEventListener('load', init);
 window.onload = init;
 function init(){
     contacts = document.getElementById("contacts");
     console.log(contacts);
     var tableContent = "<table><tr><th>Name</th><th>Mobile Number</th><th>EMail</th><th>DOB</th><th>Address</th><th>State</th></tr></table>";
-    contacts.innerHTML = tableContent;// + JSON.parse(contacts-data);
+    contacts.innerHTML = tableContent;
     var add = document.getElementById("Add");
     add.onclick = addContactPage;
-    // var update = document.getElementById("Update");
+    var update = document.getElementById("Update");
     // update.onclick = fetchJSONFile('contacts.json', function(data){
+    //     console.log(data);
+    // });
+    // update.onclick = fetchJSONFile('contact-form.html', function(data){
     //     console.log(data);
     // });
 }
 function addContactPage(){
     //window.location = "contact-form.html";
+    var att = document.createAttribute("src");
+    att.value = "contact-form.html";
+    var ht = document.createAttribute("height");
+    ht.value = "900";
+    var embe = document.createElement("embed");
+    embe.setAttributeNode(att);
+    embe.setAttributeNode(ht);
+    var divele = document.getElementById("main-div");
+    divele.appendChild(embe);
     var createContact = document.getElementById("Add-Contact");
     createContact.onclick = addContact;
+
+    // localStorage.setItem('contact', JSON.stringify(contactsObj));
 }
 function addContact(){
-
+    var _name = document.getElementById("name").value;
+    localStorage.setItem("name", _name);
+    console.log(localStorage.getItem("name"));
 }
 function fetchJSONFile(path, callback) {
     // var httpRequest = new XMLHttpRequest();
